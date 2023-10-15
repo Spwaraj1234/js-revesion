bcbe8cb622963d3ab5c0072c02d587b2c165a5fc
Spwaraj1234 <140327376+Spwaraj1234@users.noreply.github.com>
2023-10-15 17:22:52 +0000

    datatype

---
+++ 011/datatype.js
@@ -0,0 +1,10 @@
+"use strict "//treat all js code as newer version
+
+// alert(3+3) use in browser consloe
+
+console.log(3); //code readbility should high
+console.log("swaraj");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
