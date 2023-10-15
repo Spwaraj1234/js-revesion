8de5bb0f6ab36b6e5e3edf0348010fab78631c1c
Spwaraj1234 <140327376+Spwaraj1234@users.noreply.github.com>
2023-10-15 17:39:00 +0000

    operation

--- 011/conversion.js
+++ 011/conversion.js
@@ -12,3 +12,17 @@ console.log(typeof valueInNumber);
 let sc = 3
 let stringNum = String(sc)
 console.log(typeof stringNum);
+
+// operations
+
+// let val = 3
+// let negval = -val
+// console.log(negval);
+
+// console.log(3+3);
+// console.log(3%3);
+// console.log(3*3);
+
+// console.log(+true);
+
+// preincremnet decrement
