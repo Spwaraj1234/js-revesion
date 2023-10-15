a5a5e90935b61ac4fc0e05e1b2a0c2693fc99a4b
Spwaraj1234 <140327376+Spwaraj1234@users.noreply.github.com>
2023-10-15 17:31:16 +0000

    conversion

---
+++ 011/conversion.js
@@ -0,0 +1,14 @@
+let score ="33"
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" = 33
+// "33ad" = NAN
+// true  = 1 false = 0
+
+let sc = 3
+let stringNum = String(sc)
+console.log(typeof stringNum);
