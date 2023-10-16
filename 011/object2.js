21951e95bdbdecc73e3a26f3b5ef20e8dd42459a
Spwaraj1234 <140327376+Spwaraj1234@users.noreply.github.com>
2023-10-16 07:35:57 +0000

    object2

---
+++ 011/object2.js
@@ -0,0 +1,16 @@
+// const tinerUser = new Object()
+const tinderUser = {}
+tinderUser.id ="123a"
+tinderUser.name = 'swa'
+
+// console.log(tinderUser);
+
+const regularUser={
+    fullname:{
+        username:{
+            name1:"sw",
+            name2:"sa"
+        }
+    }
+}
+console.log(regularUser.fullname.username.name1);
