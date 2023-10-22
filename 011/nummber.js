d88a70245e5983ad3de5a5ed5ebd408d5c42694f
Spwaraj1234 <140327376+Spwaraj1234@users.noreply.github.com>
2023-10-22 15:36:01 +0000

    math and number

---
+++ 011/nummber.js
@@ -0,0 +1,28 @@
+// number
+
+// const score =10.89
+// console.log(typeof score.toString())
+
+// console.log(score.toPrecision(3));
+// MAX_VALUE MIN VALUE 
+
+// maths
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.8));
+
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.floor(4.2));
+
+// console.log(Math.max(1,2,3));
+
+// console.log(Math.floor(Math.random()*10+1));
+
+// const min =20;
+// const max = 40;
+
+// console.log(Math.floor(Math.random()*(max-min +1))+min)
