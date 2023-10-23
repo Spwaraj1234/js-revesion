3357ba2b60bac077774285a72c647fb3cbe55175
Spwaraj1234 <140327376+Spwaraj1234@users.noreply.github.com>
2023-10-23 12:42:52 +0000

    date and time

---
+++ 011/date.js
@@ -0,0 +1,16 @@
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+
+// let myDate = new Date(2023,2,23)
+// console.log(myDate.toDateString());
+
+// let myTime = Date.now()
+
+// console.log(myTime);
+// console.log(Math.floor(myTime/1000));
+// let myDate = new Date()
+
+// myDate.toLocaleDateString('default',{
+//     weekday: "long",
+//     timeZone:''
+// })
